13f2d1dff4218ceafaf82d37464e5e125f18eaf7
ZoologistGiraffe <coman@nampachristianschools.com>
2025-11-27 10:21:43 -0700

    lemlib

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,100 @@
 #include "main.h"
-#include "liblvgl/llemu.hpp"
-#include "pros/abstract_motor.hpp"
+#include "lemlib/api.hpp" // IWYU pragma: keep
+#include "lemlib/chassis/trackingWheel.hpp"
 #include "pros/adi.hpp"
 #include "pros/misc.h"
-#include "pros/misc.hpp"
-#include "pros/motor_group.hpp"
+#include "pros/motors.hpp"
 #include "pros/rtos.hpp"
-#include <string>
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} 
-	else {
-		pros::lcd::clear_line(2);
-	}
-}
+// controller
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+// motor groups
+pros::MotorGroup leftMotors({-2, -12, -14},
+                            pros::MotorGearset::green); // left motor group - ports 2 (reversed), 12 (reversed), 14 (reversed)
+                            
+pros::MotorGroup rightMotors({1, 11, 13}, pros::MotorGearset::green); // right motor group - ports 1, 11, 13
+
+pros::Motor lower_intake(16); 	   // Creates a motor for the lower intake
+
+pros::Motor upper_intake(15);		   // Creates a motor for the upper intake
+
+pros::Motor lower_back_intake(-3);   // Creates a motor for the lower back intake
+
+pros::Motor upper_back_intake(-4);   // Creates a motor for the upper back intake
+
+pros::adi::DigitalOut flap('h'); // Creates a group for the pistons that power the flap
+
+pros::adi::DigitalOut tracking_wheel('g');  // Creates a piston for the tracking wheel
+
+// Inertial Sensor on port 5
+pros::Imu imu(5);
+
+// tracking wheels
+// horizontal tracking wheel encoder. Rotation sensor, port 7, reversed
+pros::Rotation horizontalEnc(-7);
+// vertical tracking wheel encoder. Rotation sensor, port 8, reversed
+pros::Rotation verticalEnc(-8);
+// horizontal tracking wheel. 2" diameter, 7.5" offset, back of the robot (negative)
+lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_2, -7.5);
+// vertical tracking wheel. 2" diameter, 1.625" offset, right of the robot (positive)
+lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_2, 1.625);
+
+// drivetrain settings
+lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
+                              &rightMotors, // right motor group
+                              10.5, // 10 inch track width
+                              lemlib::Omniwheel::OLD_275, // using new 4" omnis
+                              333, // drivetrain rpm is 333 + 1/3
+                              2 // horizontal drift is 2. If we had traction wheels, it would have been 8
+);
+
+// lateral motion controller
+lemlib::ControllerSettings linearController(10, // proportional gain (kP)
+                                            0, // integral gain (kI)
+                                            3, // derivative gain (kD)
+                                            0, // anti windup
+                                            0, // small error range, in inches
+                                            0, // small error range timeout, in milliseconds
+                                            0, // large error range, in inches
+                                            0, // large error range timeout, in milliseconds
+                                            0 // maximum acceleration (slew)
+);
+
+// angular motion controller
+lemlib::ControllerSettings angularController(10, // proportional gain (kP)
+                                             0, // integral gain (kI)
+                                             100, // derivative gain (kD)
+                                             3, // anti windup
+                                             1, // small error range, in degrees
+                                             100, // small error range timeout, in milliseconds
+                                             3, // large error range, in degrees
+                                             500, // large error range timeout, in milliseconds
+                                             0 // maximum acceleration (slew)
+);
+
+// sensors for odometry
+lemlib::OdomSensors sensors(&vertical, // vertical tracking wheel
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we don't have a second one
+                            &horizontal, // horizontal tracking wheel
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+// input curve for throttle input during driver control
+lemlib::ExpoDriveCurve throttleCurve(3, // joystick deadband out of 127
+                                     10, // minimum output where drivetrain will move out of 127
+                                     1.019 // expo curve gain
+);
+
+// input curve for steer input during driver control
+lemlib::ExpoDriveCurve steerCurve(3, // joystick deadband out of 127
+                                  10, // minimum output where drivetrain will move out of 127
+                                  1.019 // expo curve gain
+);
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors, &throttleCurve, &steerCurve);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -32,117 +103,109 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
 
-	pros::lcd::register_btn1_cb(on_center_button);
+    tracking_wheel.set_value(1);   // activates the piston
+    
+
+    // the default rate is 50. however, if you need to change the rate, you
+    // can do the following.
+    // lemlib::bufferedStdout().setRate(...);
+    // If you use bluetooth or a wired connection, you will want to have a rate of 10ms
+
+    // for more information on how the formatting for the loggers
+    // works, refer to the fmtlib docs
+    // thread to for brain screen and position logging
+    pros::Task screenTask([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // log position telemetry
+            lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
+            // delay to save resources
+            pros::delay(50);
+        }
+    });
 }
 
 /**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
+ * Runs while the robot is disabled
  */
 void disabled() {}
 
 /**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
+ * runs after initialize if the robot is connected to field control
  */
 void competition_initialize() {}
 
+// get a path used for pure pursuit
+// this needs to be put outside a function
+ASSET(example_txt); // '.' replaced with "_" to make c++ happy
+
 /**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
+ * Runs during auto
  *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
+ * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-	/*
-	pros::MotorGroup left_mg({-2, -12, -14}, pros::MotorGearset::green);	// Creates a motor group with forwards ports 2, 12, and 14
-	pros::MotorGroup right_mg({1, 11, 13}, pros::MotorGearset::green);		// Creates a motor group with reversed ports 1, 11, and 13
-	pros::Motor lower_intake(15); 			   		// Creates a motor for the lower intake
-	pros::Motor upper_intake(16);						// Creates a motor for the upper intake
-	pros::Motor lower_back_intake(3);    	   			// Creates a motor for the lower back intake
-	pros::Motor upper_back_intake(4);		   			// Creates a motor for the upper back intake
 
-	left_mg.move_relative(100, 100);
-	right_mg.move_relative(100, 100);
-	while (!((left_mg.get_position() < 105) && (left_mg.get_position() > 95))) {
-		pros::delay(2);
-	}
-	left_mg.brake();
-	right_mg.brake();
-	*/
+    //chassis.setPose(0, 0, 0);
+
+    //chassis.moveToPose(0, 100, 0, 5000);
+
+    //upper_intake.move(127);
+    //pros::delay(100);
+    //upper_intake.brake();
 }
 
 /**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
+ * Runs in driver control
  */
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({-2, -12, -14}, pros::MotorGearset::green);	// Creates a motor group with forwards ports 2, 12, and 14
-	pros::MotorGroup right_mg({1, 11, 13}, pros::MotorGearset::green);		// Creates a motor group with reversed ports 1, 11, and 13
-	pros::Motor lower_intake(16); 			   		// Creates a motor for the lower intake
-	pros::Motor upper_intake(15);						// Creates a motor for the upper intake
-	pros::Motor lower_back_intake(-3);    	   			// Creates a motor for the lower back intake
-	pros::Motor upper_back_intake(-4);		   			// Creates a motor for the upper back intake
-	pros::adi::DigitalOut flap('h');			   	// Creates a group for the pistons that power the flap
-	
 
-	while (true) {
+    tracking_wheel.set_value(0); // pulls the tracking wheel up
+
+    // loop to continuously update motors
+    while (true) {
 
 
-		//autonomous();
+        // drivetrain controls
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);    // get joystick positions
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);  // get joystick positions
+        
+        chassis.arcade(leftY, rightX);  // move the chassis with curvature drive
+        
+
+        // pneumatics control
+        int flapState = controller.get_digital(pros::E_CONTROLLER_DIGITAL_A);
+
+        flap.set_value(flapState);
 
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    			// Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  			// Gets the turn left/right from right joystick
-
-		left_mg.move(dir + turn);                      			// Sets left motor voltage
-		right_mg.move(dir - turn);                     			// Sets right motor voltage
-
-
-		// Flap control system (ignore that there isnt anything here)
-
-
-
-		// Intake control system
-		int intakeFwd = master.get_digital(DIGITAL_R2);							// Gets the state of R2
-		int intakeRev = master.get_digital(DIGITAL_R1);							// Gets the state of R1
-
-		lower_intake.move(-(intakeFwd - intakeRev) * 127);					    // Sets the lower intake voltage
-		upper_intake.move((intakeFwd - intakeRev) * 127);						// Sets the upper intake voltage
-		lower_back_intake.move((intakeFwd - intakeRev) * 127);				    // Sets the lower back intake voltage
-		upper_back_intake.move((intakeFwd - intakeRev) * 127);					// Sets the upper back intake voltage
-		
-		// Debug information
-		pros::lcd::set_text(0, "Left Analog Y:  " + std::to_string(dir));							// Prints the left analog y value
-		pros::lcd::set_text(1, "Right Analog X: " + std::to_string(turn));							// Prints the right analog x value
-		pros::lcd::set_text(2, "Intake direction: " + std::to_string(intakeFwd - intakeRev));		// Prints the direction of the intake
-		
-
-		pros::delay(20);                         // Run for 20 ms then update
-	}
+        // intake control system
+        if(controller.get_digital(DIGITAL_R2)){
+            lower_intake.move(-127);
+            upper_intake.move(127);
+            lower_back_intake.move(127);
+            upper_back_intake.move(127);
+        }
+        else if (controller.get_digital(DIGITAL_R1)) {
+            lower_intake.move(127);
+            upper_intake.move(-127);
+            lower_back_intake.move(-127);
+            upper_back_intake.move(-127);
+        }
+        else {
+            lower_intake.move(0);
+            upper_intake.move(0);
+            lower_back_intake.move(0);
+            upper_back_intake.move(0);
+        }
+               
+        
+        pros::delay(10);    // delay to save resources
+    }
 }
